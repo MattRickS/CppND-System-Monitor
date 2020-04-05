7d5fe724809a8199770759c6f6c400ee68b9e5ff
MattRickS <mattalexshaw@gmail.com>
2020-04-05 18:58:54 +0100

    implement ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,16 @@
-#include <string>
-
 #include "format.h"
 
-using std::string;
+#include <iomanip>
+#include <sstream>
+#include <string>
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+const int SECONDS_IN_DAY = 3600 * 24;
+
+std::string Format::ElapsedTime(long seconds) {
+  seconds %= SECONDS_IN_DAY;
+  std::stringstream s;
+  s << std::setfill('0') << std::setw(2) << seconds / 3600 << ":"
+    << std::setw(2) << (seconds % 3600) / 60 << ":" << std::setw(2)
+    << seconds % 60;
+  return s.str();
+}
